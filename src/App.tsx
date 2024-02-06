532f71a688ffb77cab10e2ca4b3eab1035d02717
kafayat Adigun <adigunkafy27@gmail.com>
2024-02-06 11:58:52 +0100

    added missing src folder and installation of shadcn and its configuration

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Route, Routes } from "react-router-dom"
+import SignInForm from "./_auth/forms/SignInForm"
+import SignUpForm from "./_auth/forms/SignUpForm"
+import AuthLayout from "./_auth/AuthLayout"
+import RootLayout from "./root/RootLayout"
+import { Home } from "./root/pages"
+import "./global.css"
+
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+        <Routes>
+            {/* public Route */}
+            <Route element={<AuthLayout />}>
+              <Route path="/signin" element={<SignInForm />}/>
+              <Route path="/signup" element={<SignUpForm />}/>
+            </Route>
+            
+  
+            {/* private routes */}
+            <Route element={<RootLayout />}>
+              <Route index element={<Home />}/>
+            </Route>
+            
+        </Routes>
+    </main>
+  )
+}
+
+export default App
