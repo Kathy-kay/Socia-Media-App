648598ea62e82e2e50312ed513bbab9e05a57c2e
kafayat Adigun <adigunkafy27@gmail.com>
2024-02-07 20:37:26 +0100

    added icons folder and signup interface design

--- src/_auth/forms/SignUpForm.tsx
+++ src/_auth/forms/SignUpForm.tsx
@@ -1,9 +1,85 @@
-
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { signupSchema } from "@/lib/validation";
+import { z } from "zod";
 
 const SignUpForm = () => {
-  return (
-    <div>SignUpForm</div>
-  )
-}
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof signupSchema>>({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
 
-export default SignUpForm
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof signupSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assests/images/logo.svg" alt="" />
+      </div>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="John Doe" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="johnny" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="johndoe@gmail.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default SignUpForm;
---
+++ src/lib/validation/index.tsx
@@ -0,0 +1,23 @@
+import * as z from "zod"
+
+
+export const signupSchema = z.object({
+    name: z.string().min(3, {
+      message: "name must be atleast 2 characters."  
+    }),
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+    email: z.string().email(),
+    password: z.string().min(8, {
+      message: "password must be atleast 8 characters"
+    }),
+    
+  })
+
+
+  export const signinSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+  })
