bf34bb711d8476c3b5ffddc9e5c9130584e0c5d1
kafayat Adigun <adigunkafy27@gmail.com>
2024-02-15 11:15:21 +0100

    created different pages, designed the navigation bar for desktop and mobile view, design the create-post page

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import SignInForm from "./_auth/forms/SignInForm"
 import SignUpForm from "./_auth/forms/SignUpForm"
 import AuthLayout from "./_auth/AuthLayout"
 import RootLayout from "./root/RootLayout"
-import { Home } from "./root/pages"
+import { AllUsers, Bookmark, CreatePost, Explore, Home, LikedPost, PostDetails, Profile, UpdatePost, UpdateProfile } from "./root/pages"
 import "./global.css"
 import { Toaster } from "@/components/ui/toaster"
+
 
 const App = () => {
   return (
@@ -21,6 +22,16 @@ const App = () => {
             {/* private routes */}
             <Route element={<RootLayout />}>
               <Route index element={<Home />}/>
+              <Route path="/explore" element={<Explore />}/>
+              <Route path="/bookmark" element={<Bookmark />}/>
+              <Route path="/all-users" element={<AllUsers />}/>
+              <Route path="/create-post" element={<CreatePost />}/>
+              <Route path="/update-post/:id" element={<UpdatePost />}/>
+              <Route path="/post/:id" element={<PostDetails />}/>
+              <Route path="/profile/:id/*" element={<Profile />}/>
+              <Route path="/update-profile/:id" element={<UpdateProfile />}/>
+              <Route path="/liked-post/:id" element={<LikedPost/>}/>
+              
             </Route>    
         </Routes>
         <Toaster />
--- src/components/shared/TopBar.tsx
+++ src/components/shared/TopBar.tsx
@@ -2,10 +2,12 @@ import { Link, useNavigate } from "react-router-dom"
 import { Button } from "../ui/button"
 import { useSignOutAccount } from "@/lib/react-query/queryAndMutation"
 import { useEffect } from "react";
+import { useUserContext } from "@/context/AuthContext";
 
 
 const TopBar = () => {
     const {mutate: signOut, isSuccess} = useSignOutAccount();
+    const {user} = useUserContext()
 
     const navigate = useNavigate();
 
@@ -22,10 +24,19 @@ const TopBar = () => {
                 height={325}
                 />
             </Link>
-            <div className="flex gap-4">
+            <div className="flex gap-2">
                 <Button variant="ghost" className="shad-button_ghost" onClick={() => signOut()}>
                     <img src="/assets/icons/logout.svg" alt="Logout" />
                 </Button>
+                <Link to={`/profile/${user.id}`} className="flex-center gap-3">
+                    <img src={user.imageUrl || "/assets/images/profile.png"} 
+                    alt="profile-img" 
+                    width={30}
+                    height={30}
+                    className="h-8 w-8 rounded-full     "
+                    />
+                    
+                </Link>
             </div>
         </div>
     </section>
